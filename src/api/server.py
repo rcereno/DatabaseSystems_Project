fffa654e14ecf61395bb8f899034069999e603bc
iemxia <102547124+iemxia@users.noreply.github.com>
2024-05-04 21:29:07 -0700

    fixing server so we can test locally

--- src/api/server.py
+++ src/api/server.py
@@ -8,17 +8,17 @@ import sys
 from starlette.middleware.cors import CORSMiddleware
 
 description = """
-Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+REDX is the premier ecommerce site for all your gaming.
 """
 
 app = FastAPI(
-    title="Central Coast Cauldrons",
+    title="REDX",
     description=description,
     version="0.0.1",
     terms_of_service="http://example.com/terms/",
     contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
+        "name": "Xia Webster",
+        "email": "xwebster@calpoly.edu",
     },
 )
 
@@ -53,4 +53,4 @@ async def validation_exception_handler(request, exc):
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Central Coast Cauldrons."}
+    return {"message": "Welcome to REDX."}
